0fe81b01f11dbebafe76831b1929ecd93278543c
Merlin04 <Merlin04@users.noreply.github.com>
2023-03-31 10:31:27 -0700

    Astro boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), image(), mdx(), tailwind()],
+  output: "server",
+  adapter: vercel()
+});
