feaee74795368965184550562ae8ed3df8516871
Merlin04 <Merlin04@users.noreply.github.com>
2023-10-21 14:38:20 -0700

    General updates (finally add projects section!)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-// import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
@@ -9,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [/*react(), */image(), mdx(), tailwind({
-    config:  { applyBaseStyles: false }
+    config: { applyBaseStyles: false }
   })],
   output: "server",
   adapter: vercel()
